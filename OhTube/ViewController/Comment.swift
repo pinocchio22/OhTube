ee57ed83e7a0019af8310c8d11b0a5b235f998ef
p2glet <vviian.2@gmail.com>
2023-09-04 16:14:18 +0900

    [FEAT] : create DataModel

---
+++ OhTube/ViewController/Comment.swift
@@ -0,0 +1,15 @@
+//
+//  Comment.swift
+//  OhTube
+//
+//  Created by t2023-m0056 on 2023/09/04.
+//
+
+import Foundation
+
+struct Comment {
+    var id: UUID
+    var nickName: String
+    var content: String
+    var date: String
+}
