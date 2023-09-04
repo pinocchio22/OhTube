d5892fbb8b9d0166912c472decb08832f1e75b6d
Taeshiki <durudurutta@gmail.com>
2023-09-05 00:54:58 +0900

    [FEAT] : MyPage UI 및 기능 구현

--- OhTube/ViewController/MyPageViewController.swift
+++ OhTube/ViewController/MyPageViewController.swift
@@ -7,23 +7,60 @@
 
 import UIKit
 
-class MyPageViewController: UIViewController {
 
+
+class MyPageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var nickNameLabel: UILabel!
+    
+    @IBAction func idEditButtonTapped(_ sender: Any) {
+        
+        let storyBoard = UIStoryboard(name: "RegistrationScene", bundle: nil)
+        let moveVC = storyBoard.instantiateViewController(withIdentifier: RegistraionViewController.identifier)
+        moveVC.modalPresentationStyle = .fullScreen
+        moveVC.modalTransitionStyle = .crossDissolve
+        self.present(moveVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func logoutButtonTapped(_ sender: Any) {
+        
+        let storyBoard = UIStoryboard(name: "LoginScene", bundle: nil)
+        let moveVC = storyBoard.instantiateViewController(withIdentifier: LoginViewController.identifier)
+        moveVC.modalPresentationStyle = .fullScreen
+        moveVC.modalTransitionStyle = .crossDissolve
+        self.present(moveVC, animated: true, completion: nil)
+    }
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        profileImage.layer.cornerRadius = 50
+        profileImage.layer.masksToBounds = true
+        
+        let nibName = UINib(nibName: "MyPageTableViewCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "MyPageTableViewCell")
+  
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyPageTableViewCell", for: indexPath) as! MyPageTableViewCell
+        return cell
+    }
+    
+    
+    
+    
 
 }
