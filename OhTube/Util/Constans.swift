0e4819c88107fb559e038c1082c03b6b7e1c4420
Hongsik Choi <playhong16@gmail.com>
2023-09-04 15:27:59 +0900

    DESIGN: 기본 Font 크기 및 마진 설정

---
+++ OhTube/Util/Constans.swift
@@ -0,0 +1,21 @@
+//
+//  Constans.swift
+//  OhTube
+//
+//  Created by playhong on 2023/09/04.
+//
+
+import UIKit
+
+enum Font {
+    static let menuTitleFont = UIFont.boldSystemFont(ofSize: 30)
+    static let mainTitleFont = UIFont.boldSystemFont(ofSize: 24)
+    static let contentFont = UIFont.systemFont(ofSize: 16)
+}
+
+enum Margin {
+    static let defaultPadding: Int = 20
+    static let spacing: Int = 10
+}
+
+
