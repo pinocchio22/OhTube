48587e54ba538b7cf81d2e673ea0fcab929d2ca3
p2glet <vviian.2@gmail.com>
2023-09-07 15:06:46 +0900

    [FIX] : tappedLikedButton

--- OhTube/Util/Util.swift
+++ OhTube/Util/Util.swift
@@ -34,3 +34,16 @@ struct Util {
           return nil
     }
 }
+
+class ToastView: UILabel {
+    func configure() {
+        self.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        self.textColor = UIColor.white
+        self.font = UIFont.systemFont(ofSize: 16)
+        self.textAlignment = .center
+        self.alpha = 1.0
+        self.layer.cornerRadius = 5
+        self.clipsToBounds = true
+        self.numberOfLines = 2
+    }
+}
